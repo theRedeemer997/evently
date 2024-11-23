b22b5897cfc37e262dd9ade054a1672af84e35f4
theRedeemer997 <manu71997mathew@gmail.com>
2024-11-23 13:54:56 -0500

    ⚡️  removed some lines from db connection js file

--- connectDB.js
+++ connectDB.js
@@ -1,27 +1,27 @@
 //import the mongoose package
 const mongoose = require('mongoose');
 //get the user collection
-const user = require('./model/user');
+//const user = require('./model/user');
 //get the organizer collection
-const organizer = require('./model/organizer');
+//const organizer = require('./model/organizer');
 //get the createdEvents collection
-const createdEvents = require('./model/createdevents');
+//const createdEvents = require('./model/createdevents');
 //get the approvedEvents collection
-const approvedEvents = require('./model/approvedevents');
+//const approvedEvents = require('./model/approvedevents');
 //ge the rejected events collection
-const rejectedEvents = require('./model/rejectedevents');
+//const rejectedEvents = require('./model/rejectedevents');
 //ge the rating schema
-const rating = require('./model/ratings');
+//const rating = require('./model/ratings');
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.mongoUri);
-        await user.createCollection();
-        await organizer.createCollection();
-        await createdEvents.createCollection();
-        await approvedEvents.createCollection();
-        await rejectedEvents.createCollection();
-        await rating.createCollection();
-        console.log('collections are created..');
+        // await user.createCollection();
+        // await organizer.createCollection();
+        // await createdEvents.createCollection();
+        // await approvedEvents.createCollection();
+        // await rejectedEvents.createCollection();
+        // await rating.createCollection();
+        // console.log('collections are created..');
         console.log('DB connected successfully on host ', conn.connection.host);
     } catch (error) {
         console.log('Connection failed', error.message);

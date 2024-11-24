b76b027111a416f04c9577cfc6816af9fcbd96c6
theRedeemer997 <manu71997mathew@gmail.com>
2024-11-24 03:55:17 -0500

    ✨  implemented the trending events in the home page

--- controllers/getHomePage.js
+++ controllers/getHomePage.js
@@ -2,10 +2,12 @@ const constants = require('../constants');
 const formatDateTime = require('../services/formatDataTime');
 const generateDynamicMessage = require('../services/generateDynamicMessage');
 const createdEvents = require('../model/createdevents');
+const ratings = require('../model/ratings');
 
 let lastVisitMessage = '';
 // route to get the home page
 const getHomePage = async (req, res) => {
+    const evts = await ratings.find({ Rating: 5 });
     const { isAdmin, username, loggedIn, typeOf, lastVisit, uname } =
         req.cookies;
     console.log('🚀 ~ getHomePage ~ lastVisit:', lastVisit);
@@ -21,7 +23,7 @@ const getHomePage = async (req, res) => {
     ) {
         const evts = await createdEvents.find();
         let length = evts.length;
-        res.render('home', { isAdmin, loggedIn, length });
+        res.render('home', { isAdmin, loggedIn, length, evts });
     }
     //in case of user details found in the cookie then,
     //in case of organizer details found in the cookie then,
@@ -45,12 +47,12 @@ const getHomePage = async (req, res) => {
                 maxAge: 2 * 60 * 60 * 1000,
             });
         }
-        res.render('home', { loggedIn, lastVisitMessage, typeOf });
+        res.render('home', { loggedIn, lastVisitMessage, typeOf, evts });
     }
     // in case of organiser
     // console.log(req.session);
     // console.log(req.cookies);
-    else res.render('home');
+    else res.render('home', { evts });
 };
 
 module.exports = getHomePage;
